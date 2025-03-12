5602e72ff37ec17c56e0a58b3c742b24cfb87996
SebiB24 <sebycool242@gmail.com>
2025-03-12 20:53:27 +0200

    version 1.1 (imbunatatire GUI) 1.7/2

--- src/main/java/a4/MagazinElectronice.java
+++ src/main/java/a4/MagazinElectronice.java
@@ -36,56 +36,41 @@ public class MagazinElectronice extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        // Create scenes
+        Scene produseScene = createProduseScene(primaryStage);
+        Scene comenziScene = createComenziScene(primaryStage);
 
-/// COMENZI BOX ===================================================================================================================
-        HBox ProduseBox = new HBox();
-
-        HBox leftSideBox = new HBox();
-
+        primaryStage.setTitle("Magazin App");
+        primaryStage.setScene(produseScene);
+        primaryStage.show();
+    }
+    /// PRODUSE SCENE ===============================================================================================================================
+    private Scene createProduseScene(Stage primaryStage) {
+        HBox root = new HBox();
         ObservableList<Produs> ProduseList = FXCollections.observableArrayList(service.GetProduse());
 
-        /// Creare TABEL pentru afisarea datelor
-        /// !!! ADAUGA (opens Domain to javafx.base;) IN (module-info.java) !!!!!!
-
-        TableView<Produs> ProdusTable = new TableView<>();
-        //creem cate o coloana pe rand
-        //textul din paranteze este header-ul
+        TableView<Produs> table = new TableView<>();
         TableColumn<Produs, Integer> idColumn = new TableColumn<>("ID");
         TableColumn<Produs, String> numeColumn = new TableColumn<>("Nume");
         TableColumn<Produs, Integer> pretColumn = new TableColumn<>("Pret");
         TableColumn<Produs, String> categorieColumn = new TableColumn<>("Categorie");
-
-        //specificam cum se vor completa coloanele - ce camp dintr-un
-        //obiect Musician vine pe fiecare coloana
 
         idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
         numeColumn.setCellValueFactory(new PropertyValueFactory<>("nume"));
         pretColumn.setCellValueFactory(new PropertyValueFactory<>("pret"));
         categorieColumn.setCellValueFactory(new PropertyValueFactory<>("categorie"));
 
-        //adaugam coloanele la tabel
-        ProdusTable.getColumns().add(idColumn);
-        ProdusTable.getColumns().add(numeColumn);
-        ProdusTable.getColumns().add(pretColumn);
-        ProdusTable.getColumns().add(categorieColumn);
+        table.getColumns().addAll(idColumn, numeColumn, pretColumn, categorieColumn);
+        table.setItems(ProduseList);
+        table.setMinWidth(500);
 
-        ProdusTable.setItems(ProduseList);
-        ProdusTable.setMinWidth(500);
-
-        /// Adaugare Tabel/Lista in LBox
-
-        leftSideBox.getChildren().add(ProdusTable);
-        leftSideBox.setPadding(new Insets(10, 10, 10, 10));
-
-        /// Adaugare LBox in MBox
-
-        ProduseBox.getChildren().add(leftSideBox);
-
+        ///Right Box -------------------------------------------------------------------------------
         VBox RightBox = new VBox();
 
-        Button objectButton = new Button(getOtherObject(Obiect));
-        objectButton.setMaxWidth(Double.MAX_VALUE);
-        RightBox.getChildren().add(objectButton);
+        Button switchButton = new Button("Comenzi");
+        switchButton.setOnAction(e -> primaryStage.setScene(createComenziScene(primaryStage)));
+        switchButton.setMaxWidth(Double.MAX_VALUE);
+        RightBox.getChildren().add(switchButton);
 
         GridPane labelsAndFieldsPane = new GridPane();
         labelsAndFieldsPane.setVgap(3.5);
@@ -128,88 +113,17 @@ public class MagazinElectronice extends Application {
         RightBox.setSpacing(15);
         RightBox.setPadding(new Insets(10, 10, 10, 10));
 
-        ProduseBox.getChildren().add(RightBox);
+        /// BUTTON FUNCTIONS--------------------------------------------------------------------
+        table.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
-/// COMENZI BOX ===================================================================================================================
-        HBox ComenziBox = new HBox();
-
-        HBox clLeftSideBox = new HBox();
-
-        ObservableList<Comanda> ComenziList = FXCollections.observableArrayList(service.GetComenzi());
-
-        /// Creare TABEL pentru afisarea datelor
-        /// !!! ADAUGA (opens Domain to javafx.base;) IN (module-info.java) !!!!!!
-
-        TableView<Comanda> ComenziTable = new TableView<>();
-        //creem cate o coloana pe rand
-        //textul din paranteze este header-ul
-        TableColumn<Comanda, Integer> cIdColumn = new TableColumn<>("ID");
-        TableColumn<Comanda, String> dataColumn = new TableColumn<>("Data Livrare");
-        TableColumn<Comanda, ArrayList<Produs>> produseColumn = new TableColumn<>("Lista Produse");
-
-        //specificam cum se vor completa coloanele - ce camp dintr-un
-        //obiect Musician vine pe fiecare coloana
-
-        cIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        dataColumn.setCellValueFactory(new PropertyValueFactory<>("data_livrare"));
-        produseColumn.setCellValueFactory(new PropertyValueFactory<>("produse"));
-
-        //adaugam coloanele la tabel
-        ComenziTable.getColumns().add(cIdColumn);
-        ComenziTable.getColumns().add(dataColumn);
-        ComenziTable.getColumns().add(produseColumn);
-
-        ComenziTable.setItems(ComenziList);
-        ComenziTable.setMinWidth(500);
-
-        /// Adaugare Tabel/Lista in LBox
-        clLeftSideBox.getChildren().add(ComenziTable);
-        clLeftSideBox.setPadding(new Insets(10, 10, 10, 10));
-
-        /// Adaugare LBox in MBox
-        ProduseBox.getChildren().add(clLeftSideBox);
-
-        VBox cRightBox = new VBox();
-        cRightBox.getChildren().add(objectButton);
-
-        GridPane cLabelsAndFieldsPane = new GridPane();
-        cLabelsAndFieldsPane.setVgap(3.5);
-        cLabelsAndFieldsPane.setHgap(3.5);
-        Label cIdLabel = new Label("ID");
-        Label dataLabel = new Label("Data livrare");
-        Label produseLabel = new Label("Produse");
-        TextField cIdTextField = new TextField();
-        TextField dataTextField = new TextField();
-        TextField produseTextField = new TextField();
-
-        cLabelsAndFieldsPane.add(cIdLabel, 0, 0);
-        cLabelsAndFieldsPane.add(cIdTextField, 1, 0);
-        cLabelsAndFieldsPane.add(dataLabel, 0, 1);
-        cLabelsAndFieldsPane.add(dataTextField, 1, 1);
-        cLabelsAndFieldsPane.add(produseLabel, 0, 2);
-        cLabelsAndFieldsPane.add(produseTextField, 1, 2);
-
-        cRightBox.getChildren().add(cLabelsAndFieldsPane);
-
-        cRightBox.getChildren().add(buttonBox);
-
-        cRightBox.setSpacing(15);
-        cRightBox.setPadding(new Insets(10, 10, 10, 10));
-
-        ProduseBox.getChildren().add(cRightBox);
-
-        Scene produseScene = new Scene(ProduseBox, 750, 400);
-        Scene comenziScene = new Scene(ComenziBox, 750, 400);
-        primaryStage.setTitle("Magazin App");
-        primaryStage.setScene(produseScene);
-        primaryStage.show();
-
-/// BUTTON FUNCTIONS ===================================================================================================================
-        objectButton.setOnMouseClicked(new EventHandler<>() {
+        table.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-                Obiect = getOtherObject(Obiect);
-                objectButton.setText(getOtherObject(Obiect));
+                Produs selectedProdus = (Produs) table.getSelectionModel().getSelectedItem();
+                idTextField.setText(String.valueOf(selectedProdus.getId()));
+                numeTextField.setText(selectedProdus.getNume());
+                pretTextField.setText(String.valueOf(selectedProdus.getPret()));
+                categorieTextField.setText(selectedProdus.getCategorie());
             }
         });
 
@@ -219,45 +133,8 @@ public class MagazinElectronice extends Application {
 
                 try{
                     int id = Integer.parseInt(idTextField.getText());
-                    String nume = dataTextField.getText();
-                    int pret = Integer.parseInt(produseTextField.getText());
-                    String categorie = categorieTextField.getText();
-                    service.AddProd(id, categorie, nume, pret);
-                    ProduseList.setAll(service.GetProduse());
-                }
-                catch (Exception e ){
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Eroare");
-                    alert.setContentText("A aparut o eroare: "+e.getMessage());
-                    alert.show();
-                }
-
-            }
-        });
-
-        objectButton.setOnMouseClicked(new EventHandler<>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                Obiect = getOtherObject(Obiect);
-                objectButton.setText(getOtherObject(Obiect));
-
-                if(Obiect.equals("Produse")){
-                    primaryStage.setScene(produseScene);
-                }
-                else if(Obiect.equals("Comenzi")){
-                    primaryStage.setScene(comenziScene);
-                }
-            }
-        });
-
-        addButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-
-                try{
-                    int id = Integer.parseInt(idTextField.getText());
-                    String nume = dataTextField.getText();
-                    int pret = Integer.parseInt(produseTextField.getText());
+                    String nume = numeTextField.getText();
+                    int pret = Integer.parseInt(pretTextField.getText());
                     String categorie = categorieTextField.getText();
                     service.AddProd(id, categorie, nume, pret);
                     ProduseList.setAll(service.GetProduse());
@@ -294,8 +171,8 @@ public class MagazinElectronice extends Application {
             public void handle(MouseEvent mouseEvent) {
                 try{
                     int id = Integer.parseInt(idTextField.getText());
-                    String nume = dataTextField.getText();
-                    int pret = Integer.parseInt(produseTextField.getText());
+                    String nume = numeTextField.getText();
+                    int pret = Integer.parseInt(pretTextField.getText());
                     String categorie = categorieTextField.getText();
                     service.ActProd(id, categorie, nume, pret);
                     ProduseList.setAll(service.GetProduse());
@@ -309,33 +186,104 @@ public class MagazinElectronice extends Application {
             }
         });
 
-        ProdusTable.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        root.getChildren().addAll(table, RightBox);
+        return new Scene(root, 750, 400);
+    }
 
-        ProdusTable.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+/// COMENZI SCENE ===============================================================================================================================
+    private Scene createComenziScene(Stage primaryStage) {
+        HBox root = new HBox();
+        ObservableList<Comanda> ComenziList = FXCollections.observableArrayList(service.GetComenzi());
+
+        TableView<Comanda> table = new TableView<>();
+        TableColumn<Comanda, Integer> idColumn = new TableColumn<>("ID");
+        TableColumn<Comanda, String> dataColumn = new TableColumn<>("Data Livrare");
+        TableColumn<Comanda, ArrayList<Produs>> produseColumn = new TableColumn<>("Lista Produse");
+
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        dataColumn.setCellValueFactory(new PropertyValueFactory<>("data_livrare"));
+        produseColumn.setCellValueFactory(new PropertyValueFactory<>("produse"));
+
+        table.getColumns().addAll(idColumn, dataColumn, produseColumn);
+        table.setItems(ComenziList);
+        table.setMinWidth(500);
+
+
+        ///Right Box -------------------------------------------------------------------------------
+        VBox RightBox = new VBox();
+        Button switchButton = new Button("Produse");
+        switchButton.setOnAction(e -> primaryStage.setScene(createProduseScene(primaryStage)));
+        switchButton.setMaxWidth(Double.MAX_VALUE);
+        RightBox.getChildren().add(switchButton);
+
+        GridPane cLabelsAndFieldsPane = new GridPane();
+        cLabelsAndFieldsPane.setVgap(3.5);
+        cLabelsAndFieldsPane.setHgap(3.5);
+        Label idLabel = new Label("ID");
+        Label dataLabel = new Label("Data livrare");
+        Label produseLabel = new Label("Produse");
+        TextField idTextField = new TextField();
+        TextField dataTextField = new TextField();
+        TextField produseTextField = new TextField();
+
+        cLabelsAndFieldsPane.add(idLabel, 0, 0);
+        cLabelsAndFieldsPane.add(idTextField, 1, 0);
+        cLabelsAndFieldsPane.add(dataLabel, 0, 1);
+        cLabelsAndFieldsPane.add(dataTextField, 1, 1);
+        cLabelsAndFieldsPane.add(produseLabel, 0, 2);
+        cLabelsAndFieldsPane.add(produseTextField, 1, 2);
+
+        RightBox.getChildren().add(cLabelsAndFieldsPane);
+
+        HBox buttonBox = new HBox();
+        Button addButton = new Button("Add");
+        Button deleteButton = new Button("Delete");
+        Button updateButton = new Button("Update");
+
+        buttonBox.getChildren().add(addButton);
+        buttonBox.getChildren().add(deleteButton);
+        buttonBox.getChildren().add(updateButton);
+        buttonBox.setAlignment(Pos.BASELINE_CENTER);
+        buttonBox.setSpacing(10);
+
+        RightBox.getChildren().add(buttonBox);
+
+        RightBox.setSpacing(15);
+        RightBox.setPadding(new Insets(10, 10, 10, 10));
+        ///BUTTON FUNCTIONS ------------------------------------------------------------------------------------
+        table.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        table.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
-                Produs selectedProdus = (Produs) ProdusTable.getSelectionModel().getSelectedItem();
-                idTextField.setText(String.valueOf(selectedProdus.getId()));
-                dataTextField.setText(selectedProdus.getNume());
-                produseTextField.setText(String.valueOf(selectedProdus.getPret()));
-                categorieTextField.setText(selectedProdus.getCategorie());
-            }
-        });
-
-        ComenziTable.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-
-        ComenziTable.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                Comanda selctedComanda = (Comanda) ComenziTable.getSelectionModel().getSelectedItem();
-                cIdTextField.setText(String.valueOf(selctedComanda.getId()));
+                Comanda selctedComanda = (Comanda) table.getSelectionModel().getSelectedItem();
+                idTextField.setText(String.valueOf(selctedComanda.getId()));
                 dataTextField.setText(selctedComanda.getData_livrare());
                 produseTextField.setText(String.valueOf(selctedComanda.getProduse()));
             }
         });
 
-    }
+        deleteButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                try{
+                    int id = Integer.parseInt(idTextField.getText());
+                    service.StergeCom(id);
+                    ComenziList.setAll(service.GetComenzi());
+                }
+                catch (Exception e ){
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Eroare");
+                    alert.setContentText("A aparut o eroare: "+e.getMessage());
+                    alert.show();
+                }
+            }
+        });
 
+        root.getChildren().addAll(table, RightBox);
+        return new Scene(root, 750, 400);
+    }
 
     public static void main(String[] args) {
         launch(args);
